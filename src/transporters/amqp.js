749bf4c5364b736733fd9b40a1a3d24e972329fc
Nathan Schwartz <nathan.schwartz95@gmail.com>
2020-04-07 14:31:03 -0500

    reimplement amqp transporter with amqp-connection-manager
    - Switches to confirm channels
    - Add acks for events.
    - Reconnect more gracefully if connection to a broker is lost.
    - Ack if handler rejects and failure is passed to transporter. Previously
      we were nackinig which could create an infinite loop of failures and
      prevent other messages from being processed.

--- src/transporters/amqp.js
+++ src/transporters/amqp.js
@@ -77,11 +77,13 @@ class AmqpTransporter extends Transporter {
 		// connected consumers for 2 minutes.
 		const autoDeleteQueuesAfterDefault = 2*60*1000;
 
-		opts.autoDeleteQueues =
-			opts.autoDeleteQueues === true ? autoDeleteQueuesAfterDefault :
-				typeof opts.autoDeleteQueues === "number" ? opts.autoDeleteQueues :
-					opts.autoDeleteQueues === false ? -1 :
-						autoDeleteQueuesAfterDefault;
+		opts.autoDeleteQueues = opts.autoDeleteQueues === true
+			? autoDeleteQueuesAfterDefault
+			: typeof opts.autoDeleteQueues === "number"
+				? opts.autoDeleteQueues
+				: opts.autoDeleteQueues === false
+					? -1
+					: autoDeleteQueuesAfterDefault;
 
 		// Support for multiple URLs (clusters)
 		opts.url = Array.isArray(opts.url)
@@ -93,12 +95,8 @@ class AmqpTransporter extends Transporter {
 		super(opts);
 
 		this.hasBuiltInBalancer = true;
-		this.connection = null;
-		this.channel = null;
-		this.bindings = [];
-
-		this.channelDisconnecting = false;
-		this.connectionDisconnecting = false;
+		this.topology = {};
+		this.reconnectCount = 0;
 	}
 
 	/**
@@ -106,113 +104,74 @@ class AmqpTransporter extends Transporter {
 	 *
 	 * @memberof AmqpTransporter
 	 */
-	connect(errorCallback) {
-		return new this.broker.Promise((_resolve, _reject) => {
-			let _isResolved = false;
-			const resolve = () => {
-				_isResolved = true;
-				_resolve();
-			};
-			const reject = (err) => {
-				_reject(err);
-				if (_isResolved) errorCallback(err);
-			};
-
+	connect() {
+		// This promise won't reject, but will take an indefinite amount of time to resolve.
+		return new this.broker.Promise((resolve) => {
 			let amqp;
 			try {
-				amqp = require("amqplib");
+				amqp = require("amqp-connection-manager");
 			} catch(err) {
 				/* istanbul ignore next */
-				this.broker.fatal("The 'amqplib' package is missing. Please install it with 'npm install amqplib --save' command.", err, true);
+				this.broker.fatal("The 'amqp-connection-manager' or 'amqp' package is missing. Please install it with 'npm install amqp-connection-manager amqplib --save' command.", err, true);
 			}
 
-			// Pick url
-			this.connectAttempt = (this.connectAttempt||0)+1;
-			const urlIndex = (this.connectAttempt-1) % this.opts.url.length;
-			const uri = this.opts.url[urlIndex];
-			const urlParsed = url.parse(uri);
+			// Creates a connection manager and event handlers to handle reconnections. It can be safely interacted with regardless of connection state.
+			this.connection = amqp.connect(this.opts.url, {
+				connectionOptions: Object.assign({},
+					(this.opts.socketOptions || {}),
+					{ servername: url.parse(this.opts.url[0]).hostname }
+				)
+			})
+				.on("connect", ({ url: brokerUrl }) => {
+					this.connected = true;
+					this.logger.info(`AMQP is connected to broker at ${url.parse(brokerUrl).host} (reconnectCount=${this.reconnectCount})`);
 
-			amqp.connect(uri, Object.assign({},
-				(this.opts.socketOptions || {}),
-				{ servername: urlParsed.hostname }
-			))
-				.then(connection => {
-					this.connection = connection;
-					this.logger.info("AMQP is connected.");
+					const isReconnect = this.reconnectCount !== 0;
+					this.reconnectCount += 1;
 
-					/* istanbul ignore next*/
-					connection
-						.on("error", (err) => {
-							// No need to reject here since close event will be fired after
-							// if not connected at all connection promise will be rejected
-							// this.connected = false;
-							// reject(err);
-							this.logger.error("AMQP connection error.", err);
-						})
-						.on("close", (err) => {
-							this.connected = false;
-							if (!this.connectionDisconnecting) {
-								this.logger.error("AMQP connection is closed.");
-								reject(err);
-							} else {
-								this.logger.info("AMQP connection is closed gracefully.");
-							}
-						})
-						.on("blocked", (reason) => {
-							this.logger.warn("AMQP connection is blocked.", reason);
-						})
-						.on("unblocked", () => {
-							this.logger.info("AMQP connection is unblocked.");
-						});
-
-					connection
-						.createChannel()
-						.then((channel) => {
-							this.channel = channel;
-							this.logger.info("AMQP channel is created.");
-
-							channel.prefetch(this.opts.prefetch);
-
-							/* istanbul ignore next*/
-							channel
-								.on("close", () => {
-									this.channel = null;
-									// No need to reject here since close event on connection will handle
-									// this.connected = false;
-									// reject();
-									if (!this.channelDisconnecting)
-										this.logger.warn("AMQP channel is closed.");
-									else
-										this.logger.info("AMQP channel is closed gracefully.");
-								})
-								.on("error", (err) => {
-									// No need to reject here since close event will be fired after
-									// this.connected = false;
-									// reject(err);
-									this.logger.error("AMQP channel error.", err);
-								})
-								.on("drain", () => {
-									this.logger.info("AMQP channel is drained.");
-								})
-								.on("return", (msg) => {
-									this.logger.warn("AMQP channel returned a message.", msg);
-								});
-
-							return this.onConnected();
-						})
-						.then(resolve)
-						.catch((err) => {
-							/* istanbul ignore next*/
-							this.logger.error("AMQP failed to create channel.");
-							this.connected = false;
-							reject(err);
-						});
+					return this.onConnected(isReconnect).then(() => resolve());
 				})
-				.catch((err) => {
-					/* istanbul ignore next*/
-					this.logger.warn("AMQP failed to connect!");
+				.on("disconnect", ({ err }) => {
+					if (err) this.logger.error("AMQP connection error.", err);
 					this.connected = false;
-					reject(err);
+
+					if (this.transit.disconnecting) {
+						this.logger.info("AMQP connection is closed gracefully.");
+					} else {
+						this.logger.error("AMQP connection is closed.");
+					}
+				})
+
+				.on("blocked", (reason) => {
+					this.logger.warn("AMQP connection is blocked.", reason);
+				})
+				.on("unblocked", () => {
+					this.logger.info("AMQP connection is unblocked.");
+				});
+
+			// Creates a channel wrapper with a prefetch. The setup function is called whenever a connection is established,
+			// so topology and subscriptions are preserved upon broker crashes.
+			//
+			// We will add to the setup in our subscription methods.
+			this.channel = this.connection
+				.createChannel({
+					setup: (channel) => {
+						this.logger.info(`AMQP channel is created. (prefetch=${this.opts.prefetch})`);
+						channel.prefetch(this.opts.prefetch);
+					}
+				})
+				.on("close", () => {
+					// No need to reject here since close event on connection will handle reconnection logic.
+					if (this.transit.disconnecting)
+						this.logger.info("AMQP channel is closed gracefully.");
+					else
+						this.logger.warn("AMQP channel is closed.");
+				})
+				.on("error", (err) => {
+					this.logger.error("AMQP channel error.", err);
+				})
+				.on("connect", () => {
+					this.logger.warn("AMQP channel connected.");
 				});
 		});
 	}
@@ -228,21 +187,13 @@ class AmqpTransporter extends Transporter {
 	 * Queues and Exchanges are not be deleted since they could contain important messages.
 	 */
 	disconnect() {
-		if (this.connection && this.channel && this.bindings) {
-			return this.broker.Promise.all(this.bindings.map(binding => this.channel.unbindQueue(...binding)))
-				.then(() => {
-					this.channelDisconnecting = this.transit.disconnecting;
-					this.connectionDisconnecting = this.transit.disconnecting;
-				})
-				.then(() => this.channel.close())
-				.then(() => this.connection.close())
-				.then(() => {
-					this.bindings = [];
-					this.channel = null;
-					this.connection = null;
-				})
-				.catch(err => this.logger.warn(err));
-		}
+		// removeSetup ensures that the channel stops asserting the topology, and will run our teardown functions if a connection exists.
+		// If a connection doesn't exist, teardown functions won't run and we will skip the unbinding step.
+		return this.broker.Promise.all(Object.keys(this.topology)
+			.map(key => this.channel.removeSetup(this.topology[key].setup, this.topology[key].teardown)))
+			.then(() => this.channel.close())
+			.then(() => this.connection.close())
+			.catch(err => this.logger.warn(err));
 	}
 
 	/**
@@ -314,19 +265,19 @@ class AmqpTransporter extends Transporter {
 			// If a promise is returned, acknowledge the message after it has resolved.
 			// This means that if a worker dies after receiving a message but before responding, the
 			// message won't be lost and it can be retried.
+			//
+			// Calling channel.ack is safe, even if disconnected
 			if(needAck) {
 				if (isPromise(result)) {
 					return result
 						.then(() => {
-							if (this.channel)
-								this.channel.ack(msg);
+							this.channel.ack(msg);
 						})
 						.catch(err => {
 							this.logger.error("Message handling error.", err);
-							if (this.channel)
-								this.channel.nack(msg);
+							this.channel.ack(msg);
 						});
-				} else if (this.channel) {
+				} else {
 					this.channel.ack(msg);
 				}
 			}
@@ -367,41 +318,47 @@ class AmqpTransporter extends Transporter {
 	 *
 	 */
 	subscribe(cmd, nodeID) {
-		if (!this.channel) return;
-
 		const topic = this.getTopicName(cmd, nodeID);
 
 		// Some topics are specific to this node already, in these cases we don't need an exchange.
 		if (nodeID != null) {
-			const needAck = [PACKET_REQUEST].indexOf(cmd) !== -1;
+			const needAck = [PACKET_REQUEST, PACKET_EVENT].indexOf(cmd) !== -1;
 
-			return this.channel.assertQueue(topic, this._getQueueOptions(cmd))
-				.then(() => this.channel.consume(
-					topic,
-					this._consumeCB(cmd, needAck),
-					Object.assign({ noAck: !needAck }, this.opts.consumeOptions)
-				));
-
+			this.topology[topic] = {
+				setup: (channel) =>
+					channel.assertQueue(topic, this._getQueueOptions(cmd))
+						.then(() => channel.consume(
+							topic,
+							this._consumeCB(cmd, needAck),
+							Object.assign({ noAck: !needAck }, this.opts.consumeOptions)
+						)),
+				teardown: this.broker.Promise.resolve,
+			};
+			return this.channel.addSetup(this.topology[topic].setup);
 		} else {
 			// Create a queue specific to this nodeID so that this node can receive broadcasted messages.
 			const queueName = `${this.prefix}.${cmd}.${this.nodeID}`;
-
-			// Save binding arguments for easy unbinding later.
 			const bindingArgs = [queueName, topic, ""];
-			this.bindings.push(bindingArgs);
+			this.topology[queueName] = {
+				setup: (channel) => {
+					return this.broker.Promise.all([
+						channel.assertExchange(topic, "fanout", this.opts.exchangeOptions),
+						channel.assertQueue(queueName, this._getQueueOptions(cmd)),
+					])
+						.then(() => this.broker.Promise.all([
+							channel.bindQueue(...bindingArgs),
+							channel.consume(
+								queueName,
+								this._consumeCB(cmd),
+								Object.assign({ noAck: true }, this.opts.consumeOptions)
+							)
+						]));
 
-			return this.broker.Promise.all([
-				this.channel.assertExchange(topic, "fanout", this.opts.exchangeOptions),
-				this.channel.assertQueue(queueName, this._getQueueOptions(cmd)),
-			])
-				.then(() => this.broker.Promise.all([
-					this.channel.bindQueue(...bindingArgs),
-					this.channel.consume(
-						queueName,
-						this._consumeCB(cmd),
-						Object.assign({ noAck: true }, this.opts.consumeOptions)
-					)
-				]));
+				},
+				teardown: (channel) => channel.unbindQueue(...bindingArgs),
+			};
+
+			return this.channel.addSetup(this.topology[queueName].setup);
 		}
 	}
 
@@ -413,12 +370,20 @@ class AmqpTransporter extends Transporter {
 	 */
 	subscribeBalancedRequest(action) {
 		const queue = `${this.prefix}.${PACKET_REQUEST}B.${action}`;
-		return this.channel.assertQueue(queue, this._getQueueOptions(PACKET_REQUEST, true))
-			.then(() => this.channel.consume(
-				queue,
-				this._consumeCB(PACKET_REQUEST, true),
-				this.opts.consumeOptions
-			));
+
+		this.topology[queue] = {
+			setup: (channel) => {
+				return channel.assertQueue(queue, this._getQueueOptions(PACKET_REQUEST, true))
+					.then(() => channel.consume(
+						queue,
+						this._consumeCB(PACKET_REQUEST, true),
+						this.opts.consumeOptions
+					));
+			},
+			teardown: this.broker.Promise.resolve,
+		};
+
+		return this.channel.addSetup(this.topology[queue].setup);
 	}
 
 	/**
@@ -430,12 +395,19 @@ class AmqpTransporter extends Transporter {
 	 */
 	subscribeBalancedEvent(event, group) {
 		const queue = `${this.prefix}.${PACKET_EVENT}B.${group}.${event}`;
-		return this.channel.assertQueue(queue, this._getQueueOptions(PACKET_EVENT + "LB", true))
-			.then(() => this.channel.consume(
-				queue,
-				this._consumeCB(PACKET_EVENT, true),
-				this.opts.consumeOptions
-			));
+
+		this.topology[queue] = {
+			setup: (channel) => {
+				return channel.assertQueue(queue, this._getQueueOptions(PACKET_EVENT + "LB", true))
+					.then(() => channel.consume(
+						queue,
+						this._consumeCB(PACKET_EVENT, true),
+						this.opts.consumeOptions
+					));
+			},
+			teardown: this.broker.Promise.resolve,
+		};
+		return this.channel.addSetup(this.topology[queue].setup);
 	}
 
 	/**
@@ -448,16 +420,16 @@ class AmqpTransporter extends Transporter {
 	 * @returns {Promise}
 	 */
 	send(topic, data, { balanced, packet }) {
-		/* istanbul ignore next*/
-		if (!this.channel) return this.broker.Promise.resolve();
-
-		if (packet.target != null || balanced) {
-			this.channel.sendToQueue(topic, data, this.opts.messageOptions);
-		} else {
-			this.channel.publish(topic, "", data, this.opts.messageOptions);
+		// Messages are queued in memory when not connected to a broker. Queueing heartbeat packets isn't useful, but other packeets could succeed upon reconnection.
+		if (!this.connection.isConnected() && packet.type === PACKET_HEARTBEAT) {
+			return this.broker.Promise.resolve();
 		}
 
-		return this.broker.Promise.resolve();
+		if (packet.target != null || balanced) {
+			return this.channel.sendToQueue(topic, data, this.opts.messageOptions);
+		} else {
+			return this.channel.publish(topic, "", data, this.opts.messageOptions);
+		}
 	}
 }
 
